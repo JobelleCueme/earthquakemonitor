a694d56fb107c34a0173c933ed67c07f412c15ca
JobelleCueme <jobellecueme.devsys96@gmail.com>
2025-10-01 16:12:53 +0000

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/earthquake/', // <--- add this line with your repo name
+  base: '/earthquakemonitor/', // <--- add this line with your repo name
 });
